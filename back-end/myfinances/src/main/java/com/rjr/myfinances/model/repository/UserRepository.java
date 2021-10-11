b440c40c8e689a36b39b1baf0a300864eaf62814
raulsilveirajr <raulsilveirajr@yahoo.com.br>
2021-10-11 17:14:10 -0300

    feat: -implements existsByEmail in UserRepository

--- back-end/myfinances/src/main/java/com/rjr/myfinances/model/repository/UserRepository.java
+++ back-end/myfinances/src/main/java/com/rjr/myfinances/model/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.rjr.myfinances.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rjr.myfinances.model.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	// Optional<User> findByEmail(String email);
+	// Optional<User> findByEmailAndName(String email, String name);
+	boolean existsByEmail(String email);
 }
